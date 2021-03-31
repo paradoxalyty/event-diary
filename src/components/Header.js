bacc5c2750f9054c7f5ede1c1e0195e17a6acd8e
paradoxalyty <paradoxalyty@gmail.com>
2021-03-31 15:19:44 +0200

    texts translate

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,24 +4,24 @@ import logo from '../assets/trees.png';
 import iconSmile from '../assets/smile-mouth-open.png';
 
 export const Header = () => {
-    return (
-        <header className="header">
-            <div className="logo-block">
-                <a className="logo-link" href="#">
-                    <img src={logo} className="logo" alt="logo" />
-                </a>
-                <h1>Дневник</h1>
-            </div>
-            <div className="nav">
-                <input className="search" placeholder="Поиск"></input>
-                <a className="smile-btn">
-                    <img src={iconSmile} alt="icon-smile" />
-                </a>
-            </div>
-            <div className="buttons-block">
-                <button className="common-btn change-display-btn">Список</button>
-                <button className="common-btn add-note-btn">Запись</button>
-            </div>
-        </header>
-    )
-}
+  return (
+    <header className='header'>
+      <div className='logo-block'>
+        <a className='logo-link' href='#'>
+          <img src={logo} className='logo' alt='logo' />
+        </a>
+        <h1>Diary</h1>
+      </div>
+      <div className='nav'>
+        <input className='search' placeholder='Поиск'></input>
+        <a className='smile-btn'>
+          <img src={iconSmile} alt='icon-smile' />
+        </a>
+      </div>
+      <div className='buttons-block'>
+        <button className='common-btn change-display-btn'>Notes</button>
+        <button className='common-btn add-note-btn'>Add Note</button>
+      </div>
+    </header>
+  );
+};
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,54 +1,77 @@
-import React from "react";
-import { useImages } from "./useImages";
-import { Modal } from "./Modal/Modal";
+import React from 'react';
+import { useImages } from './useImages';
+import { Modal } from './Modal/Modal';
 
-import modalExample from "../assets/modal-example.jpg";
+import modalExample from '../assets/modal-example.jpg';
 
 export const Main = () => {
-    const images = useImages();
+  const images = useImages();
 
-    const modalRef = React.useRef();
-    const openModal = () => {
-        modalRef.current.openModal();
-    }
+  const modalRef = React.useRef();
+  const openModal = () => {
+    modalRef.current.openModal();
+  };
 
-    return (
-        <main className="notes">
-            {images.data.map((image) => (
-                <div key={image.id} className="note" onClick={openModal}>
-                    <img src={image.src.medium} className="note-img" width="100%" height="auto" alt={`Provided by Pexels, photographer - ${image.photographer}`} />
-                    <div className="info-box">
-                        <span className="info-titel">Побывал в горах</span>
-                        <span className="info-date">Вт 14 янв</span>
-                        <p className="info-description">Сегодня я почувствовал детское волнение...</p>
-                    </div>
-                </div>
-            ))}
+  return (
+    <main className='notes'>
+      {images.data.map((image) => (
+        <div key={image.id} className='note' onClick={openModal}>
+          <img
+            src={image.src.medium}
+            className='note-img'
+            width='100%'
+            height='auto'
+            alt={`Provided by Pexels, photographer - ${image.photographer}`}
+          />
+          <div className='info-box'>
+            <span className='info-titel'>Visited the mountains</span>
+            <span className='info-date'>Wed Jan 14</span>
+            <p className='info-description'>
+              Today I felt a childish excitement ...
+            </p>
+          </div>
+        </div>
+      ))}
 
-            <Modal ref={modalRef} >
-                <div className="modal-box-heading">
-                    <span className="modal-titel">Побывал в горах</span>
-                    <span className="modal-date">Вт 14 янв</span>
-                </div>
-                <div className="modal-box-main">
-                    <img src={modalExample} className="modal-img" alt={`Provided by Pexels, photographer - `} />
-                    <p className="modal-description">Это бесспорная истина, недаром же мы редко доверяемся тем, кто лучше нас.
-                    Скорее уж мы избегаем их общества. Чаще всего мы исповедуемся тем, кто похож на нас и разделяет наши слабости.
-                    Мы вовсе не хотим исправляться, не стремимся к самоусовершенствованию: прежде всего нужно, чтобы нас судили
-                    со всеми нашими слабостями. Нам хочется, чтобы нас пожалели и поддержали дух наш. В общем, мы хотели бы и не
-                    считаться виновными, и не стараться очиститься. В нас недостаточно цинизма и недостаточно добродетели.
-                    У нас нет ни силы зла, ни силы добра. Вы читали Данте? Правда? Вот черт! Вы, стало быть, знаете, как это у Данте?
-                    Ведь он допускает, что ангелы были нейтральными в распре между Богом и Сатаной. Он отводит им место в преддверии,
-                    так сказать в вестибюле своего ада. Мы с вами в вестибюле, дорогой друг.Терпение? Вы, разумеется, правы. Нужно
-                    набраться терпения и ждать Страшного суда. Но, к несчастью, нам некогда, мы торопимся. Так торопимся, что мне даже
-                    пришлось стать судьей на покаянии. Однако мне сначала нужно было привести в порядок свои открытия и уладить дело
-                    с насмешками моих современников. С того вечера, когда меня позвали к ответу — а ведь меня действительно позвали, — я
-                    обязан был ответить или по крайней мере поискать ответ. Это оказалось нелегко. Я долго блуждал наугад. Но этот
-                    постоянный хохот и насмешки научили меня яснее разбираться в себе и увидеть наконец, что я совсем не прост.
-                    Вы не улыбайтесь, эта истина не так уж элементарна, как кажется. Элементарными называют такие истины, которые
-                человек открывает последними, — вот и все.</p>
-                </div>
-            </Modal>
-        </main>
-    )
+      <Modal ref={modalRef}>
+        <div className='modal-box-heading'>
+          <span className='modal-titel'>Visited the mountains</span>
+          <span className='modal-date'>Wed Jan 14</span>
+        </div>
+        <div className='modal-box-main'>
+          <img
+            src={modalExample}
+            className='modal-img'
+            alt={`Provided by Pexels, photographer - `}
+          />
+          <p className='modal-description'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
+            nostrum rerum reprehenderit perferendis unde praesentium repudiandae
+            velit dolorem, eveniet vel laborum hic labore illo? Fugit aspernatur
+            amet voluptatum eum dicta eaque sint quaerat ullam, nihil voluptate
+            modi laudantium voluptas soluta eveniet saepe quibusdam reiciendis
+            temporibus cum pariatur? Vero magni dolore provident excepturi
+            exercitationem rerum ipsum consequuntur est rem harum! Quod at
+            voluptatum animi ratione fugiat enim inventore corrupti asperiores,
+            ad non numquam, quisquam ab iure! In quos reprehenderit alias
+            deleniti, consectetur enim sit, animi autem deserunt cupiditate
+            facilis odit perferendis eos cum? Expedita tempora et, quidem, velit
+            esse at doloremque, voluptatibus optio ipsa magnam eligendi omnis
+            molestiae totam nihil perspiciatis excepturi porro nesciunt cumque
+            modi nulla. Dolores incidunt cupiditate voluptatibus dignissimos
+            amet, debitis, molestias id perspiciatis quod atque tenetur
+            quibusdam, quae impedit quidem officiis reprehenderit. Hic nostrum
+            architecto illum culpa quam ratione repellat. Eum eius vero
+            voluptates, dolorem asperiores dolores? Ullam porro molestiae
+            debitis animi accusamus ducimus ipsum, pariatur qui sequi temporibus
+            maxime vel aliquam exercitationem, quia tenetur dolorem dolores aut
+            nihil dolorum magnam cum quam. Delectus amet molestias cumque eaque
+            rem sit aliquam consequuntur, quis at praesentium. Aspernatur,
+            quasi? Animi ducimus asperiores atque temporibus nam odio eius
+            veritatis pariatur?
+          </p>
+        </div>
+      </Modal>
+    </main>
+  );
 };
