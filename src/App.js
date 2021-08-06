b2ebc50dbb6dad39ac128145ddc552e69994ea00
paradoxalyty <paradoxalyty@gmail.com>
2021-08-06 16:09:25 +0200

    some refactoring in App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 // import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { HashRouter, Route } from 'react-router-dom';
-import { connect } from 'react-redux';
 
 import Main from './Pages/Main/Main';
 import NewNote from './Pages/NewNote/NewNote';
 import './App.css';
 
-class App extends Component {
-  render() {
-    // const { history } = this.props;
-    return (
-      // Since BrowserRouter is not supported on GH-Pages, I had to use HashRouter.
+const App = () => {
+  return (
+    // Since BrowserRouter is not supported on GH-Pages, I had to use HashRouter.
 
-      // <BrowserRouter>
-      //   <Switch>
-      //     <Route history={history} path='/new-note' component={NewNote} />
-      //     <Route history={history} path='/event-diary' component={Main} />
-      //   </Switch>
-      // </BrowserRouter>
+    // <BrowserRouter>
+    //   <Switch>
+    //     <Route path='/' exact component={Main} />
+    //     <Route path='/new-note' component={NewNote} />
+    //   </Switch>
+    // </BrowserRouter>
 
-      <HashRouter>
-        <Route exact path='/' component={Main} />
-        <Route path='/new-note' component={NewNote} />
-      </HashRouter>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return { history: state.history };
+    <HashRouter>
+      <Route path='/' exact component={Main} />
+      <Route path='/new-note' component={NewNote} />
+    </HashRouter>
+  );
 };
 
-const mapDispatchToProps = () => {
-  return {};
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
